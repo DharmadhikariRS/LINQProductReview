c6b795b84d7aaa04d7f9de587e9cef77bb949924
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-10-02 17:55:22 +0530

    Get-Count-By-ProductId

--- LINQoperations/Program.cs
+++ LINQoperations/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Prodcut Review Management!!!!!!!!");
             Console.WriteLine("Adding a Prodcut Review In list");
-            Console.WriteLine("Enter Option: " + "\n1.Add Values to list" + "\n2.Retrieve Top 3 Records" + "\n3.Retrieve Records Based On Rating and Product Id");
+            Console.WriteLine("Enter Option: " + "\n1.Add Values to list" + "\n2.Retrieve Top 3 Records" + "\n3.Retrieve Records Based On Rating and Product Id"+ "\n 4.Retrived the count of productId");
             int option = Convert.ToInt32(Console.ReadLine());
             //Creating a list for Product Review 
             List<ProductReview> productReviews = new List<ProductReview>();
@@ -20,6 +20,9 @@
                     break;
                 case 3:
                     ProductReviewManager.RetrieveRecordsBasedOnRatingAndProductId(productReviews);
+                    break;
+                case 4:
+                    ProductReviewManager.CountingProductId(productReviews);
                     break;
                 default:
                     Console.WriteLine("Enter correct number");
