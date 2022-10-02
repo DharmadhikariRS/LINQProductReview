cbb74b09c6cd65d3fce28faaf9ffd8ead4d37212
DharmadhikariRS <rushikesh1998sdl@gmail.com>
2022-10-02 17:37:30 +0530

    RetriveTop-3-Rating-products

--- LINQoperations/ProductReviewManager.cs
+++ LINQoperations/ProductReviewManager.cs
@@ -11,17 +11,17 @@ namespace LINQoperations
         //UC1 Add product details.
         public static void AddingProductReview(List<ProductReview> products)
         {
-            products.Add(new ProductReview() { productId = 1, userId = 1, review = "Good", rating = 14, isLike = true });
-            products.Add(new ProductReview() { productId = 2, userId = 2, review = "Average", rating = 12, isLike = true });
-            products.Add(new ProductReview() { productId = 3, userId = 4, review = "Good", rating = 19, isLike = true });
-            products.Add(new ProductReview() { productId = 2, userId = 5, review = "Bad", rating = 7, isLike = false });
-            products.Add(new ProductReview() { productId = 1, userId = 1, review = "Very Good", rating = 19, isLike = true });
-            products.Add(new ProductReview() { productId = 2, userId = 6, review = "Average", rating = 10, isLike = true });
-            products.Add(new ProductReview() { productId = 4, userId = 7, review = "Good", rating = 15, isLike = true });
-            products.Add(new ProductReview() { productId = 9, userId = 8, review = "Average", rating = 11, isLike = true });
-            products.Add(new ProductReview() { productId = 3, userId = 9, review = "Bad", rating = 6, isLike = false });
-            products.Add(new ProductReview() { productId = 5, userId = 4, review = "Average", rating = 13, isLike = true });
-            DisplayRecord(products);
+            products.Add(new ProductReview() { productId = 1, userId = 1, review = "Good", rating = 4, isLike = true });
+            products.Add(new ProductReview() { productId = 2, userId = 2, review = "Average", rating = 3, isLike = true });
+            products.Add(new ProductReview() { productId = 3, userId = 4, review = "Good", rating = 4, isLike = true });
+            products.Add(new ProductReview() { productId = 2, userId = 5, review = "Bad", rating = 1, isLike = false });
+            products.Add(new ProductReview() { productId = 1, userId = 1, review = "Very Good", rating = 5, isLike = true });
+            products.Add(new ProductReview() { productId = 2, userId = 6, review = "Average", rating = 3, isLike = true });
+            products.Add(new ProductReview() { productId = 4, userId = 7, review = "Good", rating = 4, isLike = true });
+            products.Add(new ProductReview() { productId = 9, userId = 8, review = "Average", rating = 3, isLike = true });
+            products.Add(new ProductReview() { productId = 3, userId = 9, review = "Bad", rating = 1, isLike = false });
+            products.Add(new ProductReview() { productId = 5, userId = 4, review = "Average", rating = 2, isLike = true });
+          //  DisplayRecord(products);
         }
         public static void DisplayRecord(List<ProductReview> products)
         {
@@ -30,5 +30,15 @@ namespace LINQoperations
                 Console.WriteLine("ProductId:{0}\t UserId:{1}\t Review:{2}\tRating:{3}\tIsLike:{4}\t", product.productId, product.userId, product.review, product.rating, product.isLike);
             }
         }
+        /// UC2 Retrieve Top Three Records Whose Rating is High
+        public static void RetrieveTopThreeRating(List<ProductReview> products)
+        {
+            AddingProductReview(products);
+            Console.WriteLine("\nRetrieving Top Three Records Based On Rating\n");
+            var res = (from product in products orderby product.rating descending select product).Take(3).ToList();
+            DisplayRecord(res);
+            
+        }
+        /// 
     }
 }
--- LINQoperations/Program.cs
+++ LINQoperations/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Prodcut Review Management!!!!!!!!");
             Console.WriteLine("Adding a Prodcut Review In list");
-            Console.WriteLine("Enter Option: " + "\n1.Add Values to list");
+            Console.WriteLine("Enter Option: " + "\n1.Add Values to list" + "\n2.Retrieve Top 3 Records");
             int option = Convert.ToInt32(Console.ReadLine());
             //Creating a list for Product Review 
             List<ProductReview> productReviews = new List<ProductReview>();
@@ -14,6 +14,9 @@
             {
                 case 1:
                     ProductReviewManager.AddingProductReview(productReviews);
+                    break;
+                case 2:
+                    ProductReviewManager.RetrieveTopThreeRating(productReviews);
                     break;
                 default:
                     Console.WriteLine("Enter correct number");
